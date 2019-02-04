902adfa7a1af9589c1f103ba3f44810bde30e372
bryanrtboy <bryan@bryanleister.com>
2019-02-04 12:28:39 -0700

    Created Stub Scripts

---
+++ Assets/Scripts/InputGameManager.cs
@@ -0,0 +1,52 @@
+﻿//Yourname Here, Feb, 2019
+//Based on a tutorial posted here: https://github.com/bryanrtboy/InputTutorial
+//
+//This script will be our central game manager, it should only exist once
+//in the game, aka the Singleton pattern. The script will is based on the #endregion
+//pseudocode Place Object with mouse. We need it to take a mouse position, and place
+//a target based on a raycast. Then, the frogs need to know that a target exists.
+//Secondly, we will use this to track if the frogs should start moving.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputGameManager : MonoBehaviour
+{
+    public static InputGameManager instance;
+
+    public GameObject m_targetIndicator;
+    //[HideInInspector] //Each frog will check if this is true
+    public bool m_okToMove = false;
+
+    private Vector3 m_targetPosition = Vector3.zero;
+
+    void Awake()
+    {
+        //Enforce the Singleton pattern, there can be only one InputGameManager in the scene
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check for the mouse being down, if true, activate the targetIndicator
+
+
+    }
+
+    //If the microphone is loud, change the okToMove flag to true and #endregion
+    //De-activate the indicator if it's OK to move and set the target position
+    public void ItsOKtoMove()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/ListenForAudioCommand.cs
@@ -0,0 +1,26 @@
+﻿//Yourname Here, Feb, 2019
+//Based on a tutorial posted here: https://github.com/bryanrtboy/InputTutorial
+//
+//This script will be take microphone input and check how loud it is
+//if the input volume is over X amount, we will tell the InputGameManager
+//that frogs should start moving. We could use Events and Delegates on the frogs
+//but it's simpler to just tell the Game Manager when it's OK for them to move.
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListenForAudioCommand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
